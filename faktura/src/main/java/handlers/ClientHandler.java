28f6a904192140d1f22845b5292246bdb696f5fa
Fermento99 <pk.pawlak99@gmail.com>
2019-11-03 18:33:56 +0100

    initiated handlers

---
+++ faktura/src/main/java/handlers/ClientHandler.java
@@ -0,0 +1,13 @@
+package handlers;
+
+import blocks.Client;
+
+import java.util.ArrayList;
+
+public class ClientHandler {
+    ArrayList<Client> clients;
+
+    public void createClient() {
+
+    }
+}
---
+++ faktura/src/main/java/handlers/InvoiceHandler.java
@@ -0,0 +1,11 @@
+package handlers;
+
+import blocks.Invoice;
+
+import java.util.ArrayList;
+
+public class InvoiceHandler {
+    ArrayList<Invoice> invoices;
+
+
+}
