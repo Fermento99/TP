5c9bb07c9ee0473b5d07e4f76a37245110c5cb86
Fermento99 <pk.pawlak99@gmail.com>
2019-11-04 12:46:47 +0100

    wrote handlers

--- faktura/src/main/java/handlers/ClientHandler.java
+++ faktura/src/main/java/handlers/ClientHandler.java
@@ -1,13 +1,24 @@
 package handlers;
 
+import blocks.Address;
 import blocks.Client;
 
 import java.util.ArrayList;
 
 public class ClientHandler {
-    ArrayList<Client> clients;
+    private ArrayList<Client> clients;
 
-    public void createClient() {
+    public Client createClient(final String name, final String surname, final Address address, final int pesel) {
+        try {
+            Client c = new Client(name, surname, address, pesel);
+            clients.add(c);
+            return c;
+        }
+        catch (Exception ex) { System.out.println("ClientHandler: Exception caught"); }
+        return null;
+    }
 
+    public ArrayList<Client> getClients() {
+        return clients;
     }
 }
--- faktura/src/main/java/handlers/InvoiceHandler.java
+++ faktura/src/main/java/handlers/InvoiceHandler.java
@@ -1,11 +1,20 @@
 package handlers;
 
+import blocks.Client;
 import blocks.Invoice;
+import blocks.InvoiceElement;
 
 import java.util.ArrayList;
+import java.util.Date;
 
 public class InvoiceHandler {
-    ArrayList<Invoice> invoices;
+    private ArrayList<Invoice> invoices = null;
 
+    public void createInvoice(final Date deliveryDate, final int id, final Client client, final InvoiceElement[] invoiceElements) {
+        invoices.add(new Invoice(deliveryDate, id, client, invoiceElements));
+    }
 
+    public ArrayList<Invoice> getInvoices() {
+        return invoices;
+    }
 }
